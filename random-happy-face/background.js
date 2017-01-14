2432fcd2a010b2fa9a438ecbf6c2c11fe018dda2
Tung Thanh Le <ttungl@users.noreply.github.com>
2017-01-14 00:08:00 -0600

    Update background.js

--- random-happy-face/background.js
+++ random-happy-face/background.js
@@ -1,7 +1,3 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 var min = 1; // min number of icons.
 var max = 23; // max number of icons.
 
