451d6073c4fb86cdbc0ef4f95dfedc3e4326ad85
Tung Thanh Le <ttungl@users.noreply.github.com>
2017-01-14 13:58:57 -0600

    Add files via upload

--- random-happy-face/background.js
+++ random-happy-face/background.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 var min = 1; // min number of icons.
 var max = 23; // max number of icons.
 
